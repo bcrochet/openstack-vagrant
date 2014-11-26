9e62558216e81b9f72a425b3b8aa2921b07a1881
Gilles Dubreuil <gilles@redhat.com>
2014-11-26 11:06:25 +1100

    Added puppetdb and epel repos common scripts

---
+++ common/puppetdb.sh
@@ -0,0 +1,42 @@
+# Puppetdb setup
+echo "Snipet puppetdb"
+puppetdb_server=$1
+yum -y install puppetdb puppetdb-terminus augeas facter
+
+augtool set /files/etc/puppet/puppet.conf/master/storeconfigs true
+augtool set /files/etc/puppet/puppet.conf/master/storeconfigs_backend puppetdb
+
+cat > /etc/puppet/puppetdb.conf <<EOF
+[main]
+  server = ${puppetdb_server}
+  port = 8081
+EOF
+
+cat > /etc/puppet/routes.yaml <<EOF
+---
+master:
+  facts:
+    terminus: puppetdb
+    cache: yaml
+EOF
+
+function rhel6() {
+  chkconfig puppetdb on
+  service puppetdb start
+}
+
+function rhel7() {
+  systemctl enable puppetdb
+  systemctl start puppetdb
+}
+
+case $(facter operatingsystemmajrelease) in
+  6)
+    rhel6
+    ;;
+  7|20|21)
+    rhel7
+    ;;
+  *)
+    ;;
+esac
